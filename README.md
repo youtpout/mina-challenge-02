57b7de64cbe48e727a88853da60a5f4114b506b4
Youtpout <EddyBoughioul@hotmail.fr>
2024-02-28 17:46:28 +0100

    add test and readme

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Mina zkApp: Mina Challenge 02
 
-This template uses TypeScript.
+The message list has fixed size of 200 elements, and autofill incomplete array with empty Message. It's more optimized than use recursive proof mechanism.
+
+I use Uint32 value to optimize space and verification, with Uint32 we don't need to check if value is superior to 0
+
+I use Provable if and gadget and to check if value is correct
+
+On Ryzen 7950 X I need between 6 and 8s to execute the analyze.
 
 ## How to build
 
--- src/MessageAnalyzer.test.ts
+++ src/MessageAnalyzer.test.ts
@@ -76,6 +76,32 @@ describe('MessageAnalyzer', () => {
     expect(updatedNum).toEqual(Field(99));
   });
 
+  it('correctly test messages 200', async () => {
+    await localDeploy();
+
+    const messages: Message[] = [];
+    for (let index = 0; index < 200; index++) {
+      let message = new Message({ messageNumber: new Field(index), agentId: new UInt32(1500), agentXLocation: new UInt32(15000), agentYLocation: new UInt32(20000), checksum: new UInt32(36500) });
+      //message.checksum = message.agentId.add(message.agentXLocation).add(message.agentYLocation);
+
+      messages.push(message);
+    }
+
+    const msg200 = new Message200({ messages });
+
+    // update transaction
+    const txn = await Mina.transaction(senderAccount, () => {
+      //zkApp.update();
+      zkApp.analyze(msg200);
+    });
+    await txn.prove();
+    await txn.sign([senderKey]).send();
+
+    const updatedNum = zkApp.maxMessageNumber.get();
+    expect(updatedNum).toEqual(Field(199));
+  });
+
+
   it('correctly test messages unordered', async () => {
     await localDeploy();
 
@@ -109,5 +135,52 @@ describe('MessageAnalyzer', () => {
     const updatedNum = zkApp.maxMessageNumber.get();
     expect(updatedNum).toEqual(Field(32));
   });
+
+
+
+  it('only incorrect message', async () => {
+    await localDeploy();
+
+    const messages: Message[] = [];
+
+    // invalid checksum
+    let message = new Message({ messageNumber: new Field(10), agentId: new UInt32(1500), agentXLocation: new UInt32(15000), agentYLocation: new UInt32(20000), checksum: new UInt32(50000) });
+    messages.push(message);
+
+    // Y bigger than X
+    message = new Message({ messageNumber: new Field(32), agentId: new UInt32(10), agentXLocation: new UInt32(10000), agentYLocation: new UInt32(8000), checksum: new UInt32(18010) });
+    messages.push(message);
+
+    // agent id > 3000
+    message = new Message({ messageNumber: new Field(55), agentId: new UInt32(5000), agentXLocation: new UInt32(0), agentYLocation: new UInt32(20000), checksum: new UInt32(25000) });
+    messages.push(message);
+
+    // agent X > 15001
+    message = new Message({ messageNumber: new Field(12), agentId: new UInt32(1), agentXLocation: new UInt32(15001), agentYLocation: new UInt32(20000), checksum: new UInt32(35002) });
+    messages.push(message);
+
+    // agent Y > 20000
+    message = new Message({ messageNumber: new Field(12), agentId: new UInt32(1), agentXLocation: new UInt32(5000), agentYLocation: new UInt32(20001), checksum: new UInt32(25002) });
+    messages.push(message);
+
+    // agent Y < 5000
+    message = new Message({ messageNumber: new Field(12), agentId: new UInt32(1), agentXLocation: new UInt32(0), agentYLocation: new UInt32(4000), checksum: new UInt32(4001) });
+    messages.push(message);
+
+
+    const msg200 = new Message200({ messages });
+
+    // update transaction
+    const txn = await Mina.transaction(senderAccount, () => {
+      //zkApp.update();
+      zkApp.analyze(msg200);
+    });
+    await txn.prove();
+    await txn.sign([senderKey]).send();
+
+    const updatedNum = zkApp.maxMessageNumber.get();
+    expect(updatedNum).toEqual(Field(0));
+  });
+
 
 });
